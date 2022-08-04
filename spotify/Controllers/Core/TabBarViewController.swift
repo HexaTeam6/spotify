7c6ef04c45f61e62d3a600c8721176fdf700f60f
HexaTeam6 <abdurrachmanwahed@gmail.com>
2022-08-04 22:57:45 +0700

    feat: add tab bar fungsionality

--- spotify/Controllers/Core/TabBarViewController.swift
+++ spotify/Controllers/Core/TabBarViewController.swift
@@ -11,7 +11,32 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let homeViewController = HomeViewController()
+        let searchViewController = SearchViewController()
+        let libraryViewController = LibraryViewController()
+        
+        homeViewController.title = "Browse"
+        searchViewController.title = "Search"
+        libraryViewController.title = "Library"
+        
+        homeViewController.navigationItem.largeTitleDisplayMode = .always
+        searchViewController.navigationItem.largeTitleDisplayMode = .always
+        libraryViewController.navigationItem.largeTitleDisplayMode = .always
+        
+        let homeNavigationController = UINavigationController(rootViewController: homeViewController)
+        let searchNavigationController = UINavigationController(rootViewController: searchViewController)
+        let libraryNavigationController = UINavigationController(rootViewController: libraryViewController)
+        
+        homeViewController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
+        searchViewController.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        libraryViewController.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "music.note.list"), tag: 1)
+        
+        homeNavigationController.navigationBar.prefersLargeTitles = true
+        searchNavigationController.navigationBar.prefersLargeTitles = true
+        libraryNavigationController.navigationBar.prefersLargeTitles = true
+        
+        setViewControllers([homeNavigationController, searchNavigationController, libraryNavigationController], animated: false)
     }
     
 
